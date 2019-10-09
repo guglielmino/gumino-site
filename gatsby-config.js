61f8c473580ccca649c9a372b1c4d9995e3fcbf4
Fabrizio Guglielmino <guglielmino@gmail.com>
2019-10-09 16:23:42 +0200

    Fixed embedding image in markdown post

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,33 +72,38 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-
-    
-    `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-images`,
+            resolve: 'gatsby-remark-relative-images',
+            options: {
+              name: 'uploads',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-images',
             options: {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
-              maxWidth: 590,
+              maxWidth: 2048,
             },
           },
           {
-            resolve: `gatsby-source-filesystem`,
+            resolve: 'gatsby-remark-copy-linked-files',
             options: {
-              path: `${__dirname}/src/images`,
+              destinationDir: 'static',
             },
           },
         ],
       },
     },
+
+
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
   ],
 }
